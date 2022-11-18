98be6c8121f129e1a06e04fa1f15f4d1db24836b
Gabriela Lopez <gabriela.lopez@scanntech.com>
2022-11-18 11:27:23 -0300

    create hero module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
+import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    HeroesModule,
+    CounterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
